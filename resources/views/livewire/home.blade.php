f24fa5ccc95e3df2c13eca565c20b70787404796
Rohmat Hidayat <840002@telkom.co.id>
2023-10-19 16:29:38 +0800

    update home navigation

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,25 +1,25 @@
 <div class="space-y-6">
     <div class="grid grid-cols-2 lg:grid-cols-4 gap-4">
         <div class="stats shadow w-full">
-            <div class="stat">
+            <a href="{{ route('order.queue') }}" class="stat">
                 <div class="stat-title">Masih antri</div>
                 <div class="stat-value">{{ $datas->where('status', 'queue')->count() }}</div>
                 <div class="stat-desc">Dalam antrian</div>
-            </div>
+            </a>
         </div>
         <div class="stats shadow w-full">
-            <div class="stat">
+            <a href="{{ route('order.progress') }}" class="stat">
                 <div class="stat-title">On Progress</div>
                 <div class="stat-value">{{ $datas->where('status', 'progress')->count() }}</div>
                 <div class="stat-desc">Sedang diproses</div>
-            </div>
+            </a>
         </div>
         <div class="stats shadow w-full">
-            <div class="stat">
+            <a href="{{ route('order.done') }}" class="stat">
                 <div class="stat-title">Selesai diantar</div>
                 <div class="stat-value">{{ $datas->where('status', 'done')->count() }}</div>
                 <div class="stat-desc">Selesai diantar</div>
-            </div>
+            </a>
         </div>
         <div class="stats shadow w-full">
             <div class="stat">
