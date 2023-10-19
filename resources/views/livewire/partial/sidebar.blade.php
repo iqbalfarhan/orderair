bfacb02839c293cf95ebcfc6da60fa38ac94bb73
Rohmat Hidayat <840002@telkom.co.id>
2023-10-19 15:39:27 +0800

    update active status

--- resources/views/livewire/partial/sidebar.blade.php
+++ resources/views/livewire/partial/sidebar.blade.php
@@ -3,7 +3,7 @@
         <h2 class="menu-title">Halaman utama</h2>
         <ul>
             <li>
-                <a href="{{ route('home') }}" wire:navigate>
+                <a href="{{ route('home') }}" class="{{ Route::is('home') ? 'active' : '' }}" wire:navigate>
                     <x-icon name="dashboard" />
                     <span>Dashboard</span>
                 </a>
@@ -15,27 +15,27 @@
         <h2 class="menu-title">Pemesanan</h2>
         <ul>
             <li>
-                <a href="{{ route('order.create') }}" wire:navigate>
+                <a href="{{ route('order.create') }}" class="{{ Route::is('order.create') ? 'active' : '' }}" wire:navigate>
                     <x-icon name="circle-plus" />
                     <span>Tambah antrian</span>
                 </a>
             </li>
             <li>
-                <a href="{{ route('order.queue') }}" wire:navigate>
+                <a href="{{ route('order.queue') }}" class="{{ Route::is('order.queue') ? 'active' : '' }}" wire:navigate>
                     <x-icon name="hourglass" />
                     <span>Masih mengantri</span>
                     <span class="badge badge-sm">{{ $datas->where('status', 'queue')->count() }}</span>
                 </a>
             </li>
             <li>
-                <a href="{{ route('order.progress') }}" wire:navigate>
+                <a href="{{ route('order.progress') }}" class="{{ Route::is('order.progress') ? 'active' : '' }}" wire:navigate>
                     <x-icon name="garden-cart" />
                     <span>Dalam pengiriman</span>
                     <span class="badge badge-sm">{{ $datas->where('status', 'progress')->count() }}</span>
                 </a>
             </li>
             <li>
-                <a href="{{ route('order.done') }}" wire:navigate>
+                <a href="{{ route('order.done') }}" class="{{ Route::is('order.done') ? 'active' : '' }}" wire:navigate>
                     <x-icon name="checkbox" />
                     <span>Selesai diantar</span>
                     <span class="badge badge-sm">{{ $datas->where('status', 'done')->count() }}</span>
