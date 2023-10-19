c496e29c76eb838f2274a2c88b6418d5bb29b904
Rohmat Hidayat <840002@telkom.co.id>
2023-10-19 16:10:28 +0800

    update wire poll

--- resources/views/livewire/partial/sidebar.blade.php
+++ resources/views/livewire/partial/sidebar.blade.php
@@ -1,4 +1,4 @@
-<ul class="menu p-4 w-80 min-h-full bg-base-100 text-base-content space-y-4" wire:poll>
+<ul class="menu p-4 w-80 min-h-full bg-base-100 text-base-content space-y-4">
     <li>
         <h2 class="menu-title">Halaman utama</h2>
         <ul>
@@ -24,21 +24,21 @@
                 <a href="{{ route('order.queue') }}" class="{{ Route::is('order.queue') ? 'active' : '' }}" wire:navigate>
                     <x-icon name="hourglass" />
                     <span>Masih mengantri</span>
-                    <span class="badge badge-sm">{{ $datas->where('status', 'queue')->count() }}</span>
+                    <span class="badge badge-sm" wire:poll>{{ $datas->where('status', 'queue')->count() }}</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('order.progress') }}" class="{{ Route::is('order.progress') ? 'active' : '' }}" wire:navigate>
                     <x-icon name="garden-cart" />
                     <span>Dalam pengiriman</span>
-                    <span class="badge badge-sm">{{ $datas->where('status', 'progress')->count() }}</span>
+                    <span class="badge badge-sm" wire:poll>{{ $datas->where('status', 'progress')->count() }}</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('order.done') }}" class="{{ Route::is('order.done') ? 'active' : '' }}" wire:navigate>
                     <x-icon name="checkbox" />
                     <span>Selesai diantar</span>
-                    <span class="badge badge-sm">{{ $datas->where('status', 'done')->count() }}</span>
+                    <span class="badge badge-sm" wire:poll>{{ $datas->where('status', 'done')->count() }}</span>
                 </a>
             </li>
         </ul>
