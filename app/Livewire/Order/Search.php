e047492aa2fdb5581667d51474413d7b7267d9c6
Rohmat Hidayat <840002@telkom.co.id>
2023-10-19 15:56:09 +0800

    update build

--- app/Livewire/Order/Search.php
+++ app/Livewire/Order/Search.php
@@ -2,14 +2,19 @@
 
 namespace App\Livewire\Order;
 
+use App\Models\Order;
 use Livewire\Component;
 
 class Search extends Component
 {
-
     public $cari;
+
     public function render()
     {
-        return view('livewire.order.search');
+        return view('livewire.order.search', [
+            'datas' => $this->cari ? Order::when($this->cari, function ($q) {
+                return $q->where('id', $this->cari)->orWhere('name', "like", '%' . $this->cari . '%');
+            })->get() : null
+        ]);
     }
 }
